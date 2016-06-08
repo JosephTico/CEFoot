cc899b093c69d60f695de073cd4f977953f84ede
Joseph Vargas <joseph@cpokemon.com>
2016-06-08 14:06:38 -0600

    Limpieza de código

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, QtMultimedia
 from design import *
 from about import *
-import sys, time
+import sys
+import time
 import serial
 
 
-
-#Probando un comentario
-#Otro comentarito
+# Inicializa la conexión a Arduno
 arduino = None
 try:
     arduino = serial.Serial("COM5", 9600)
@@ -15,16 +14,16 @@ except:
     pass
 
 
-class Interfaz(QtWidgets.QMainWindow, Ui_MainWindow):
-
+class VentanaTitulo(QtWidgets.QMainWindow, Ui_VentanaTitulo):
     def __init__(self, parent=None):
-        super(Interfaz, self).__init__(parent)
+        super(VentanaTitulo, self).__init__(parent)
+
         self.setupUi(self)
+
         self.play.clicked.connect(self.jugar)
         self.exit.clicked.connect(self.salir)
         self.buttonNextsong.clicked.connect(self.nextsong)
-        self.pushButton_2.clicked.connect(self.about)
-
+        self.botonAbout.clicked.connect(self.abreAbout)
 
     def jugar(self):
         global arduino
@@ -32,18 +31,19 @@ class Interfaz(QtWidgets.QMainWindow, Ui_MainWindow):
             arduino = serial.Serial("COM5", 9600)
         except:
             pass
-        if arduino:
-            QtWidgets.QMessageBox.critical(self, 'Error',"No se ha podido establecer la conexión con el dispositivo de juego.", QtWidgets.QMessageBox.Ok)
+        if not arduino:
+            QtWidgets.QMessageBox.critical(
+                self, 'Error', "No se ha podido establecer la conexión con el dispositivo de juego.", QtWidgets.QMessageBox.Ok)
         else:
-            #Otro comentario por acá
+            # Otro comentario por acá
             arduino.write(b'9')
 
     def reproduceMusica(self):
-        self.url2= QtCore.QUrl.fromLocalFile("./audio/title2.mp3")
-        self.url= QtCore.QUrl.fromLocalFile("./audio/title.mp3")
+        self.musica2 = QtCore.QUrl.fromLocalFile("./audio/title2.mp3")
+        self.musica1 = QtCore.QUrl.fromLocalFile("./audio/title.mp3")
         self.playlist = QtMultimedia.QMediaPlaylist()
-        self.playlist.addMedia(QtMultimedia.QMediaContent(self.url2))
-        self.playlist.addMedia(QtMultimedia.QMediaContent(self.url))
+        self.playlist.addMedia(QtMultimedia.QMediaContent(self.musica2))
+        self.playlist.addMedia(QtMultimedia.QMediaContent(self.musica1))
         self.playlist.setPlaybackMode(QtMultimedia.QMediaPlaylist.Loop)
         self.playlist.setCurrentIndex(1)
         self.player = QtMultimedia.QMediaPlayer()
@@ -51,41 +51,36 @@ class Interfaz(QtWidgets.QMainWindow, Ui_MainWindow):
         self.player.setVolume(100)
         self.player.play()
 
-
     def salir(self):
         sys.exit()
-                                   
+
     def nextsong(self):
         self.playlist.next()
 
-
-    def about(self):
-        self.aboutUi = aboutWindow()
+    def abreAbout(self):
+        self.aboutUi = VentanaAbout()
         self.aboutUi.show()
 
 
-
-class aboutWindow(QtWidgets.QMainWindow, Ui_Form):
-    
+class VentanaAbout(QtWidgets.QMainWindow, Ui_VentanaAbout):
     def __init__(self, parent=None):
-        super(aboutWindow, self).__init__(parent)
+        super(VentanaAbout, self).__init__(parent)
         self.setupUi(self)
 
-        
-    
+# Inicializa el programa
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    
+
     splash_pic = QtGui.QPixmap('./images/loading.png')
-    splash = QtWidgets.QSplashScreen(splash_pic, QtCore.Qt.WindowStaysOnTopHint)
+    splash = QtWidgets.QSplashScreen(
+        splash_pic, QtCore.Qt.WindowStaysOnTopHint)
     splash.setMask(splash_pic.mask())
     splash.show()
 
-
-
-    juego = Interfaz()    
-
+    juego = VentanaTitulo()
     juego.reproduceMusica()
     juego.show()
+
     splash.finish(juego)
+
     sys.exit(app.exec_())
